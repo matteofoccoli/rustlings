1d258b0f51ba0432c2b1d74d42bfcb730db2c3d8
matteo <matteo.foccoli@gmail.com>
2022-10-18 21:32:47 +0200

    initial commit after a few exercises

--- exercises/hashmaps/hashmaps2.rs
+++ exercises/hashmaps/hashmaps2.rs
@@ -11,8 +11,6 @@
 //
 // Execute `rustlings hint hashmaps2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 #[derive(Hash, PartialEq, Eq)]
@@ -37,6 +35,7 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // TODO: Put new fruits if not already present. Note that you
         // are not allowed to put any type of fruit that's already
         // present!
+        basket.entry(fruit).or_insert(1);
     }
 }
 
@@ -56,7 +55,9 @@ mod tests {
     #[test]
     fn test_given_fruits_are_not_modified() {
         let mut basket = get_fruit_basket();
+        
         fruit_basket(&mut basket);
+        
         assert_eq!(*basket.get(&Fruit::Apple).unwrap(), 4);
         assert_eq!(*basket.get(&Fruit::Mango).unwrap(), 2);
         assert_eq!(*basket.get(&Fruit::Lychee).unwrap(), 5);
@@ -65,7 +66,9 @@ mod tests {
     #[test]
     fn at_least_five_types_of_fruits() {
         let mut basket = get_fruit_basket();
+        
         fruit_basket(&mut basket);
+        
         let count_fruit_kinds = basket.len();
         assert!(count_fruit_kinds >= 5);
     }
@@ -73,7 +76,9 @@ mod tests {
     #[test]
     fn greater_than_eleven_fruits() {
         let mut basket = get_fruit_basket();
+        
         fruit_basket(&mut basket);
+        
         let count = basket.values().sum::<u32>();
         assert!(count > 11);
     }
--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -16,9 +16,7 @@
 // - The input is going to be a Vector of a 2-length tuple,
 //   the first element is the string, the second one is the command.
 // - The output element is going to be a Vector of strings.
-// No hints this time!
-
-// I AM NOT DONE
+// Execute `rustlings hint quiz2` or use the `hint` watch subcommand for a hint.
 
 pub enum Command {
     Uppercase,
@@ -27,14 +25,26 @@ pub enum Command {
 }
 
 mod my_module {
-    use super::Command;
+    use super::Command; // sibling has to be imported
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
+            match command {
+                Command::Uppercase => output.push(string.to_uppercase()),
+                Command::Trim => output.push(string.trim().to_string()),
+                Command::Append(times) => {
+                    let mut result = string.clone();
+                    result.push_str(&"bar".repeat(*times));
+                    // for n in 0..*size {
+                    //     result.push_str("bar");
+                    // }
+                    output.push(result)
+                }
+            }
         }
         output
     }
@@ -43,7 +53,7 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we have to import to have `transformer` in scope?
-    use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
