31c92b3609f51245343124f69fb6bf6de31cc26c
matteo <matteo.foccoli@gmail.com>
2022-11-16 08:21:39 +0100

    solves first 2 exercises on conversions

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -35,10 +35,22 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        let mut p = Person::default();
+        let vec: Vec<&str> = s.split(',').collect();
+        if vec.len() != 2 {
+            return p;
+        }
+        if vec[0].is_empty() {
+            return p;
+        }
+        if vec[1].parse::<usize>().is_err() {
+            return p;
+        }
+        p.name = vec[0].to_string();
+        p.age = vec[1].parse::<usize>().unwrap();
+        return p;
     }
 }
 
